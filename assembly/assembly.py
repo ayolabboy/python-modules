b2f06e545ead91694757cd32f612e07e6d1653ff
Dongik <dishin@jlkgroup.com>
2020-07-21 17:03:55 +0900

    asseummyy

---
+++ assembly/assembly.py
@@ -0,0 +1,7 @@
+from capstone import *
+
+CODE = b"\x31\xd2\x52\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x52\x53\x89\xe1\x8d\x42\x0b\xcd\x80"
+
+md = Cs(CS_ARCH_X86, CS_MODE_32)
+for i in md.disasm(CODE, 0x1000):
+    print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
